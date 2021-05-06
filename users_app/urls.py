9e16db3f75f02e7805a9cde75bfbfba2be580cad
Elena Zelenskaya <elena.a.zelenskaya@gmail.com>
2021-05-06 13:18:45 +0300

    add updating users

--- users_app/urls.py
+++ users_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("add_user/", views.add_user),
-    path("delete/<int:id>", views.delete_user)
+    path("delete/<int:id>/", views.delete_user),
+    path("edit/<int:id>/", views.edit_user),
+    path("update/<int:id>/", views.update_user),
 ]
--- users_app/views.py
+++ users_app/views.py
@@ -20,3 +20,23 @@ def add_user(request):
 def delete_user(request, id):
     User.objects.get(id=id).delete()
     return redirect("/")
+
+def edit_user(request, id):
+    context = {
+        "user_to_edit": User.objects.get(id=id)
+	}
+    return render(request, "edit.html", context)
+
+def update_user(request, id):
+    user_to_update = User.objects.get(id=id)
+    if request.method == "POST":
+        if request.POST["fname"]:
+            user_to_update.first_name = request.POST["fname"]
+        if request.POST["lname"]:
+            user_to_update.last_name = request.POST["lname"]
+        if request.POST["email"]:
+            user_to_update.email_address = request.POST["email"]
+        if request.POST["age"]:
+            user_to_update.age = request.POST["age"]
+        user_to_update.save()
+    return redirect("/")
